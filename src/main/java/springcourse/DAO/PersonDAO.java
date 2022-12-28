461d4a0f18eed9ff850aee6797f245c4afde560d
Wex <bernikcooldude@yandex.ru>
2022-12-28 20:49:40 +0300

    @ModelAttribute

--- src/main/java/springcourse/DAO/PersonDAO.java
+++ src/main/java/springcourse/DAO/PersonDAO.java
@@ -10,14 +10,15 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private List<Person> peopleList;
+    private static int PEOPLE_COUNT;
 
     {
         peopleList = new ArrayList<>();
-        peopleList.add(new Person(1, "Tom"));
-        peopleList.add(new Person(2, "Nick"));
-        peopleList.add(new Person(3, "Jack"));
-        peopleList.add(new Person(4, "Sam"));
-        peopleList.add(new Person(5, "Hugo"));
+        peopleList.add(new Person(++PEOPLE_COUNT, "Tom"));
+        peopleList.add(new Person(++PEOPLE_COUNT, "Nick"));
+        peopleList.add(new Person(++PEOPLE_COUNT, "Jack"));
+        peopleList.add(new Person(++PEOPLE_COUNT, "Sam"));
+        peopleList.add(new Person(++PEOPLE_COUNT, "Hugo"));
     }
 
     public List<Person> getPeopleList() {
@@ -26,5 +27,10 @@ public class PersonDAO {
 
     public Person showById(int id) {
         return peopleList.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        peopleList.add(person);
     }
 }
--- src/main/java/springcourse/controllers/PeopleController.java
+++ src/main/java/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import springcourse.DAO.PersonDAO;
+import springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -30,4 +29,16 @@ public class PeopleController {
         model.addAttribute("person", personDAO.showById(id));
         return "people/showById";
     }
+
+    @GetMapping("/new")
+    public String addNewPerson(@ModelAttribute("person") Person person) {
+        return "people/newPerson";
+    }
+
+    @PostMapping
+    public String createNewPerson(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
 }
