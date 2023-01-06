94641c8d6348c069edf19a5dbe71618950c07098
Wex <bernikcooldude@yandex.ru>
2023-01-06 23:14:54 +0300

    jdbc template

--- src/main/java/springcourse/SpringCourseApplication.java
+++ src/main/java/springcourse/SpringCourseApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import javax.sql.DataSource;
+import java.sql.DriverManager;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringCourseApplication {
@@ -12,6 +17,23 @@ public class SpringCourseApplication {
     @Bean
     HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         return new HiddenHttpMethodFilter();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/springcourse");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("1234");
+
+        return  dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 
     public static void main(String[] args) {
